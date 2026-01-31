326a7817aaee4c35e9ecd96201b0ac5ebf4f9b71
Digoox99 <rodrigo99santiago@gmail.com>
2026-01-31 15:58:31 -0300

    Cria uma Entidade no BD a partir de NinjaModel

---
+++ src/main/java/dev/rodrigo/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,19 @@
+package dev.rodrigo.CadastroDeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Seja bem vindo!";
+    }
+
+
+}
