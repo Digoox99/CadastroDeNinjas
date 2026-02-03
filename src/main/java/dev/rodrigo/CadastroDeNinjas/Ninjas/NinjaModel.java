98b84d68d2e0b942a9482a71ba869660f2684a3d
Digoox99 <rodrigo99santiago@gmail.com>
2026-02-03 11:15:13 -0300

    feat: Inclui Lombok na Classe para abstração

--- src/main/java/dev/rodrigo/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/rodrigo/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,11 +1,18 @@
 package dev.rodrigo.CadastroDeNinjas.Ninjas;
 import dev.rodrigo.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 //transformando classe em Entidade
 @Entity
 @Table(name="tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,37 +26,4 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public NinjaModel(){
-
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
