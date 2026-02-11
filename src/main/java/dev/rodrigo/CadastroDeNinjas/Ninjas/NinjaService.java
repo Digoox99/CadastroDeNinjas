fddb4b974c4bc30b1e891f6af96c6e9607398a09
Digoox99 <rodrigo99santiago@gmail.com>
2026-02-11 10:39:22 -0300

    feat: cria construtor, dependência NinjaRepository e método para listar Ninjas

--- src/main/java/dev/rodrigo/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/rodrigo/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,22 @@
 package dev.rodrigo.CadastroDeNinjas.Ninjas;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Listar todos os Ninjas
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+
 }
