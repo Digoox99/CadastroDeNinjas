b04cd5f9e95ab1a00cf9ff91e0f285b2eda7a188
Digoox99 <rodrigo99santiago@gmail.com>
2026-02-14 12:18:43 -0300

    fix:@Data no MissoesModel

--- src/main/java/dev/rodrigo/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/rodrigo/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,12 +3,14 @@ package dev.rodrigo.CadastroDeNinjas.Missoes;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.rodrigo.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 @Entity
 @Table(name="tb_missoes")
 public class MissoesModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
